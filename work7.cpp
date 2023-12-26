d17089bcbaa6bcd89e1d83789c8acfd0a55f11f1
blackwhite0625 <f0989724842@gmail.com>
2023-12-26 15:09:49 +0800

    add work7.cpp

--- work7.cpp
+++ work7.cpp
@@ -10,7 +10,15 @@ struct data{
     char name[10];
     char gender;
     int math;
-};
+}
+
+st[2]={{"bird",'M',90},{"sky",'F',78}};
+
+for(int i=0;i<2;i++){
+    cout << st[i].name <<endl;
+    cout << st[i].gender <<endl;
+    cout << st[i].math <<endl;
+}
 
 struct data student1;
 strcpy(student1.name,"andy");
@@ -26,6 +34,15 @@ cout << student1.math <<endl;
 cout << student2.name <<endl;
 cout << student2.gender <<endl;
 cout << student2.math <<endl;
+
+struct data st3;
+gets(st3.name);
+scanf("%c",&st3.gender);
+fflush(stdin);
+scanf("%d",&st3.math);
+cout << st3.name <<endl;
+cout << st3.gender <<endl;
+cout << st3.math <<endl;
 
 system("pause");
 return 0;
